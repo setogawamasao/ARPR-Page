cd8728cc891963138f9a8187294c6ba74c7109b3
kgami3 <65150238+kgami3@users.noreply.github.com>
2022-04-14 22:36:30 +0900

    neeeeeeemuuuuuuui

--- replacePath.sh
+++ replacePath.sh
@@ -2,3 +2,4 @@ echo "<<replace to deploy for github>>"
 grep -l 'javascripts' ./docs/*.html | xargs sed -i -e 's/javascripts/ARPR-Page\/javascripts/g'
 grep -l 'stylesheets' ./docs/*.html | xargs sed -i -e 's/stylesheets/ARPR-Page\/stylesheets/g'
 grep -l 'images' ./docs/*.html | xargs sed -i -e 's/images/ARPR-Page\/images/g'
+grep -l 'images' ./docs/stylesheets/*.css | xargs sed -i -e 's/images/ARPR-Page\/images/g'
