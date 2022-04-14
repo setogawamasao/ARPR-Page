c4155978038d6f07b1888d973c456868560f8eaf
kgami3 <65150238+kgami3@users.noreply.github.com>
2022-04-14 22:48:53 +0900

    sentakumonoooooo

--- replacePath.sh
+++ replacePath.sh
@@ -3,3 +3,5 @@ grep -l 'javascripts' ./docs/*.html | xargs sed -i -e 's/javascripts/ARPR-Page\/
 grep -l 'stylesheets' ./docs/*.html | xargs sed -i -e 's/stylesheets/ARPR-Page\/stylesheets/g'
 grep -l 'images' ./docs/*.html | xargs sed -i -e 's/images/ARPR-Page\/images/g'
 grep -l 'images' ./docs/stylesheets/*.css | xargs sed -i -e 's/images/ARPR-Page\/images/g'
+grep -l 'index.html' ./docs/*.html | xargs sed -i -e 's/index\.html/ARPR-Page\/index.html/g'
+grep -l 'privacypolicy.html' ./docs/*.html | xargs sed -i -e 's/privacypolicy.html/ARPR-Page\/privacypolicy.html/g'
